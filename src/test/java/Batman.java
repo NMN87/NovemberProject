8e2e421d854abd69a05c1b7cc8d059ba149f49ae
NMN87 <milos.nikolic87@yahoo.com>
2020-11-01 11:24:10 -0500

    Batman is devojcica

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -5,6 +5,7 @@ public class Batman {
         System.out.println("I will catch you Joker");
         //But maybe he will not catch him
         // Batman has cool Batmobile
+        // BAtman je picka
     }
 
 }
